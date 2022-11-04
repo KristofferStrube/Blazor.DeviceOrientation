bb49a9500c57b3f89f453f2443896dfc1c7891fd
KristofferStrube <KristofferStrube@users.noreply.github.com>
2022-11-04 15:39:26 +0000

    deploy: 1175b113953d42bcbdbf9c0b3a2005afc7a575d5

---
+++ _content/KristofferStrube.Blazor.DeviceOrientation/KristofferStrube.Blazor.DeviceOrientation.js
@@ -0,0 +1,26 @@
+export function addListenerForDeviceOrientation(objRef) {
+    if (typeof DeviceMotionEvent.requestPermission === "function") {
+        DeviceMotionEvent.requestPermission().then(response => {
+            if (response == 'granted') {
+                window.addEventListener('deviceorientation', (e) => deviceOrientationListener(e, objRef));
+            }
+        });
+    }
+    else {
+        Promise.all([navigator.permissions.query({ name: "accelerometer" }),
+        navigator.permissions.query({ name: "gyroscope" })])
+            .then((results) => {
+                if (results.every((result) => result.state === "granted")) {
+                    window.addEventListener('deviceorientation', (e) => deviceOrientationListener(e, objRef));
+                }
+            });
+    }
+}
+
+export function removeListenerForDeviceOrientation(objRef) {
+    window.removeEventListener('deviceorientation', (e) => deviceOrientationListener(e, objRef));
+}
+
+function deviceOrientationListener(e, objRef) {
+    objRef.invokeMethod("InvokeOnDeviceOrientation", {alpha: e.alpha, beta: e.beta, gamma: e.gamma, absolute: e.absolute});
+}
